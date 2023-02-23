9bb649092912233d58a5a8a4bc8990b820853ba4
c-johns51 <cjohns51@uccs.edu>
2023-02-23 16:42:51 -0700

    Update JohnsonCameronAssignment2_CS2300.java

--- CS2300Project1/JohnsonCameronAssignment2_CS2300.java
+++ CS2300Project1/JohnsonCameronAssignment2_CS2300.java
@@ -6,19 +6,18 @@ import java.lang.Math;
 
 public class JohnsonCameronAssignment2_CS2300 {
 	
-
-	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 		File inputFile = new File("p2-1.txt");
 		Scanner readFile = new Scanner(inputFile);
 		
+		// Array initialization
 		char[][] gameBoard = createMatrix(inputFile, readFile);
 		ArrayList<Integer> playedCoordinates = new ArrayList<>();
 		int[] invalidCoordinates = new int[4];
 		
-		
+		// Variable initialization
 		int invalidTimeout = readFile.nextInt();
 		int invalidTurnCounter = 0;
 		
@@ -27,10 +26,15 @@ public class JohnsonCameronAssignment2_CS2300 {
 		int endRow = 0;
 		int endCol = 0;
 		
+		// While file still has items to be read
 		while(readFile.hasNext()) {
 			
+			// Goes back and forth between player 1 and 2
 			for(int player = 1; player <= 2; player++) {
 				
+				// If invalidTurnCounter is greater than 0, decrement invalidTurnCounter
+				// This makes it so that the amount of turns a set of coordinates can't
+				// be played for goes down every turn
 				if(invalidTurnCounter > 0) {
 					
 					invalidTurnCounter--;
@@ -49,11 +53,14 @@ public class JohnsonCameronAssignment2_CS2300 {
 				
 					boolean isTurnValid = validateTurn(startRow, startCol, endRow, endCol, playedCoordinates, invalidCoordinates, invalidTurnCounter);
 				
+					// If the turn is valid
 					if(isTurnValid) {
 					
 						processPlay(gameBoard, startRow, startCol, endRow, endCol, player);
 						printPlay(gameBoard);
 						
+						// Adds the played coordinates to a list of played coordinates at the
+						// end of the turn
 						playedCoordinates.add(startRow);
 						playedCoordinates.add(startCol);
 						playedCoordinates.add(endRow);
@@ -62,9 +69,13 @@ public class JohnsonCameronAssignment2_CS2300 {
 					}
 					else {
 					
+						// Sets invalidTurnCounter to the value of invalidTimeout
 						invalidTurnCounter = invalidTimeout;
 						System.out.println("This play was invalid. You cannot play these points for the next " + invalidTurnCounter + " turns.");
 						
+						// Sets played coordinates as coordinates that can't be played
+						// I'm not sure if this assignment requires multiple invalid coordinates or
+						// just one set, so I'm doing one set because it's less complicated
 						invalidCoordinates[0] = startRow;
 						invalidCoordinates[1] = startCol;
 						invalidCoordinates[2] = endRow;
@@ -78,10 +89,12 @@ public class JohnsonCameronAssignment2_CS2300 {
 			
 		}
 		
+		// Prints the final score
 		printTotals(gameBoard);
 			
 	}
 	
+	// Method that creates the board given the size from the file
 	public static char[][] createMatrix(File inputFile, Scanner readFile) throws IOException {
 		
 		int boardSize = readFile.nextInt();
@@ -96,33 +109,58 @@ public class JohnsonCameronAssignment2_CS2300 {
  		
 	}
 	
+	// Method that validates the turn, and sends true or false back to main
 	public static boolean validateTurn(int sr, int sc, int er, int ec, ArrayList<Integer> previousPlays, int[] invalidCoordinates, int invalidTurns) {
 		
 		boolean result = true;
+		int perpendicularDotProduct = 0;
 		
-		int[] currentPoints = {sr, sc, er, ec};
-		
+		// If there are coordinates in the previousPlays array list
+		// This may not be needed anymore but I will leave it here because
+		// the code runs the same regardless
 		if(previousPlays.size() != 0) {
-			for(int i = 0; i < previousPlays.size(); i++) {
-				for(int j = 0; j < currentPoints.length; j++) {
+			
+			// This is a weird for loop because I can't quite describe what I was doing.
+			// I think basically I wanted to iterate through previousPlays 0-3, then 4-7
+			// and so on and so fourth. So i is for each set of 4 values, and then the math done
+			// when getting values gets specific needed values based on the i value.
+			for(int i = 0; i < previousPlays.size() / 4; i++) {
 				
-					if(currentPoints[j] == previousPlays.get(j + (i * 2))) {
-					
-						result = false;
-					
-					}
+				int[] previousStart = {previousPlays.get(i * 4), previousPlays.get((i * 4) + 1)};
+				int[] previousEnd = {previousPlays.get((i * 4) + 2), previousPlays.get((i * 4) + 3)};
 				
+				// Creates the previous line based on the points
+				int[] previousLine = {previousEnd[0] - previousStart[0], previousEnd[1] - previousStart[1]};
+				
+				// If the current start point or end point match a previous start or end point,
+				// result is false
+				if(previousStart[0] == sr && previousStart[1] == sc) {
+					
+					result = false;
+					
+				}
+				else if(previousEnd[0] == er && previousEnd[1] == ec) {
+					
+					result = false;
+					
+				}
+				
+				// Calculates the dot product between the current line and the previous line
+				perpendicularDotProduct = previousLine[0] * (er - sr) + previousLine[1] * (ec - sc);
+				
+				// If the dot product is 0, meaning the lines are perpendicular
+				// result is false
+				if(perpendicularDotProduct == 0) {
+					
+					result = false;
+					
 				}
 				
 			}
 			
 		}
-		else {
-			
-			result = true;
-			
-		}
 		
+		// If there are still turns making a set of points invalid, test current points against those points
 		if(invalidTurns > 0) {
 			
 			int[] invalidStart = {invalidCoordinates[0], invalidCoordinates[1]};
@@ -148,6 +186,7 @@ public class JohnsonCameronAssignment2_CS2300 {
 		
 	}
 	
+	// Method that processes the play
 	public static void processPlay(char[][] gameBoard, int sr, int sc, int er, int ec, int player) {
 		
 		// A boat load of variables
@@ -236,10 +275,12 @@ public class JohnsonCameronAssignment2_CS2300 {
 		
 	}
 	
+	// Method that prints the play
 	public static void printPlay(char[][] gameBoard) {
 		
 		System.out.print("  ");
 		
+		// Prints out the numbers of the columns of the board
 		for(int i = 1; i < gameBoard.length; i++) {
 			
 			System.out.printf("%d ", i);
@@ -248,6 +289,9 @@ public class JohnsonCameronAssignment2_CS2300 {
 		
 		System.out.println();
 		
+		// Prints out the numbers of the rows of the board, backwards
+		// because normally 2D arrays are top to bottom by default
+		// but we need bottom to top
 		for(int i = gameBoard.length - 1; i > 0; i--) {
 			
 			System.out.print(i + " ");
@@ -266,19 +310,24 @@ public class JohnsonCameronAssignment2_CS2300 {
 		
 	}
 	
+	// Method to print totals after the game
 	public static void printTotals(char[][] gameBoard) {
 		
 		int player1Count = 0;
 		int player2Count = 0;
 		
+		// Iterates through the 2D array
 		for(int i = 1; i < gameBoard.length; i++) {
-			for(int j = 1; i < gameBoard[0].length; j++) {
+			for(int j = 1; j < gameBoard[0].length; j++) {
 				
+				// If current value is X, increment player1Count
 				if(gameBoard[i][j] == 'X') {
 					
 					player1Count++;
 					
 				}
+		
+				// If current value is 0, increment player2Count
 				else if(gameBoard[i][j] == 'O') {
 					
 					player2Count++;
@@ -290,14 +339,25 @@ public class JohnsonCameronAssignment2_CS2300 {
 			
 		}
 		
+		// Prints how many of each there were
+		System.out.println("There were " + player1Count + " X's placed and " + player2Count + " O's placed.");
+		
+		// If more X than O
 		if(player1Count > player2Count) {
 			
 			System.out.println("Player 1 wins!");
 			
 		}
+		// If more O than X
 		else if(player2Count > player1Count) {
 			
 			System.out.println("Player 2 wins!");
+			
+		}
+		// If same amount of X and O
+		else if(player1Count == player2Count) {
+			
+			System.out.println("There was a tie!");
 			
 		}
 		
